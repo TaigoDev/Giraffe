d7bbbdcdde8993e06c09750514d0f64e5de21ce2
Nikita Burykin <118153916+f0rsy@users.noreply.github.com>
2024-08-23 00:06:47 +0500

    redy main page without adaptive

--- Giraffe/wwwroot/js/site.js
+++ Giraffe/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+let currentSlide = 0;
+
+function changeSlide(n) {
+    const slides = document.querySelector('.slides');
+    const totalSlides = slides.children.length;
+
+    currentSlide = (currentSlide + n + totalSlides) % totalSlides;
+    slides.style.transform = `translateX(${-currentSlide * 100}%)`;
+}
